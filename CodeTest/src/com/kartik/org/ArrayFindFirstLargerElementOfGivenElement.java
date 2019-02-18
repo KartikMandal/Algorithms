f3d172ac8c8511d028ac66a2072b91925b63a746
Kartik Chandra Mandal <kartik.cse43@gmail.com>
2019-02-18 22:47:08 +0530

    Using Binary search

--- CodeTest/src/com/kartik/org/ArrayFindFirstLargerElementOfGivenElement.java
+++ CodeTest/src/com/kartik/org/ArrayFindFirstLargerElementOfGivenElement.java
@@ -36,9 +36,84 @@ public class ArrayFindFirstLargerElementOfGivenElement {
 	 
 	    return result;
 	}
+	
+	
+	
+	
+	
+	static class Node {  
+	    int key;  
+	    Node left, right;  
+	} 
+	  
+	// To create new BST Node  
+	static Node newNode(int item)  
+	{  
+	    Node temp = new Node();  
+	    temp.key = item;  
+	    temp.left = null; 
+	    temp.right = null;  
+	    return temp;  
+	}  
+	  
+	// To insert a new node in BST  
+	static Node insert(Node node, int key)  
+	{  
+	    // if tree is empty return new node  
+	    if (node == null)  
+	        return newNode(key);  
+	  
+	    // if key is less then or grater then  
+	    // node value then recur down the tree  
+	    if (key < node.key)  
+	        node.left = insert(node.left, key);  
+	    else if (key > node.key)  
+	        node.right = insert(node.right, key);  
+	  
+	    // return the (unchanged) node pointer  
+	    return node;  
+	}  
+	  
+	// function to find max value less then N  
+	static int findMaxforN(Node root, int N)  
+	{  
+	    // Base cases  
+	    if (root == null)  
+	        return -1;  
+	    if (root.key == N)  
+	        return N;  
+	    else if (root.key> N)  
+            return root.key;  
+	    return root.key < N?findMaxforN(root.right, N):findMaxforN(root.left, N);
+	}
 	public static void main(String[] args) {
 		int[] a = {10, 20, 20, 30, 40, 50};
-System.out.println(findNextHigher(a,35));
+		System.out.println(findNextHigher(a,35));
+		
+		
+		int N = 41;
+	    Node root1 = null; 
+	    root1 = insert(root1, a[0]);
+	    for (int i=1;i<a.length;i++) {
+	    	 insert(root1, a[i]);
+		}
+	      
+	    System.out.println(findMaxforN(root1, N));
+	    
+	    Node root = null; 
+	    root = insert(root, 25);  
+	    insert(root, 2);  
+	    insert(root, 1);  
+	    insert(root, 3);  
+	    insert(root, 12);  
+	    insert(root, 9);  
+	    insert(root, 21);  
+	    insert(root, 19);  
+	    insert(root, 25);  
+	  
+	    System.out.println(findMaxforN(root, N));
+	    
+	    System.out.println(findMaxforN(root, 19));
 	}
 
 }
